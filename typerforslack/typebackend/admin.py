11cd8e8bdd1a1fc82f3bebc50a423c6f09f0843e
TheMightyRaider <sivasanjay7@gmail.com>
2020-04-29 23:17:33 +0530

    Added TypingRecord model

--- typerforslack/typebackend/admin.py
+++ typerforslack/typebackend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TypingRecord
 
 # Register your models here.
+
+class TypingAdmin(admin.ModelAdmin):
+    list_display=['user_id','speed']
+
+admin.site.register(TypingRecord,TypingAdmin)
--- typerforslack/typebackend/models.py
+++ typerforslack/typebackend/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator
+
 
 # Create your models here.
 
-class User(models.Model):
-    user_name=models.CharField(max_length=100)
-    password=models.CharField(max_length=12)
-    email_id=models.EmailField('Email Address')
-
+class TypingRecord(models.Model):
+    user_id=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    speed=models.IntegerField(db_column='Typing Speed',validators=[MaxValueValidator(3)])
+    typeddate=models.DateTimeField('Practised On')
+    
+    def __str__(self):
+        return self.speed
