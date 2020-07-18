48417a2a417596bb89749ea116954675f4c45ca9
TheMightyRaider <sivasanjay7@gmail.com>
2020-07-18 15:51:34 +0530

    Added signup/signin for google user

--- typebackend/urls.py
+++ typebackend/urls.py
@@ -6,7 +6,9 @@ from . import typingviews
 urlpatterns=[
     path('',views.index.as_view(),name='index'),
     path('api-token-auth/',obtain_auth_token,name='login_token'),
+    path('login-with-google',views.LoginForGoogleUsers.as_view(),name='google_login'),
     path('register',views.Register.as_view(),name='register'),
+    path('register-for-google-user',views.RegisterForGoogleUsers.as_view(),name='register_for_google_user'),
     path('validate-username',views.UserNameAvailability.as_view(),name='validate_username'),
     path('logout',views.Logout.as_view(),name='logout'),
     path('userlog',typingviews.PostSpeed.as_view(),name='userlog'),
--- typebackend/views.py
+++ typebackend/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.renderers import JSONRenderer
-from rest_framework import status
-from rest_framework.response import Response
-from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from google.auth.transport import requests
+from rest_framework.views import APIView
+from django.shortcuts import render
+from google.oauth2 import id_token
+from rest_framework import status
+from decouple import config
+from random import randint
+import time
+
 
 # Create your views here.
 
@@ -38,6 +45,33 @@ class Register(APIView):
             return Response({'success':True,'token':user.key})
         else:
             return Response({'success':False,'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+
+class RegisterForGoogleUsers(APIView):
+    def post(self,request):
+        token=request.data['token']
+        username=request.data['username']
+        id_info=id_token.verify_oauth2_token(token,requests.Request(),config('CLIENT_ID'))
+        email=id_info['email']
+        password=hash(email)*randint(0,int(round(time.time() * 1000)))
+        data={'email':email,'username':username,'password':password}
+        serializer=RegisterSerializer(data=data)
+        if serializer.is_valid():
+            user=serializer.save()
+            return Response({'success':True,'token':user.key})
+        else:
+            return Response({'success':False,'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+
+class LoginForGoogleUsers(APIView):
+    def post(self,request):
+        token=request.data['token']
+        id_info=id_token.verify_oauth2_token(token,requests.Request(),config('CLIENT_ID'))
+        email=id_info['email']
+        user_details=User.objects.get(email=email)
+        if user_details:
+            token=Token.objects.create(user=user_details)
+            return Response({'token':token.key})
+        else:
+            return Response({'error':'Invalid Credential'},status=status.HTTP_400_BAD_REQUEST)
 
 class Logout(APIView):
     permission_classes=[IsAuthenticated]
